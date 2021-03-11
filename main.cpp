f7fa1183395e5061cc225f3ff10fc472605d8c1e
avianinfluenza <64621106+avianinfluenza@users.noreply.github.com>
2021-03-11 12:23:15 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ const int M_input_demension_y = 10;
 
 
 float weight[M_layer][M_perceptron_x][M_perceptron_y][M_input_demension_x][M_input_demension_y];
+float bais[M_layer][M_perceptron_x][M_perceptron_y];
+float input[M_input_demension_x][M_input_demension_y];
 
 double sigmoid(double x){
 	return 1/(1+pow(E, -1*x));
