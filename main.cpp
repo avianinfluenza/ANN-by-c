00ff80ada6a1ec4f17e27aff363491c4d4919974
avianinfluenza <64621106+avianinfluenza@users.noreply.github.com>
2021-03-11 12:06:06 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+#define E 2.718281
+
+const int M_layer = 10;
+const int M_perceptron_x = 10;
+const int M_perceptron_y = 10;
+const int M_input_demension_x = 10;
+const int M_input_demension_y = 10;
+
+
+float weight[M_layer][M_perceptron_x][M_perceptron_y][M_input_demension_x][M_input_demension_y];
+
+double sigmoid(double x){
+	return 1/(1+pow(E, -1*x));
+}
+
+
+int main(){
+	printf("%lf", sigmoid(3));
+}
