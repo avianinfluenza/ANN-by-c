5e8ebba4599ca415dd6e1b2ea1d58fb380b34dfe
avianinfluenza <64621106+avianinfluenza@users.noreply.github.com>
2021-03-19 12:07:40 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,16 +12,17 @@ const int M_perceptron_y = 10;
 const int M_input_demension_x = 10;
 const int M_input_demension_y = 10;
 const int M_input_n = 100;
+int layer, perceptron_x, perceptron_y, input_demension_x, input_demension_y, input_n, layerxy[M_layer][2]; //[0]이 x축, [1]이 y축 
 
 
-float weight[M_layer][M_perceptron_x][M_perceptron_y][M_input_demension_x][M_input_demension_y];
+float weight[M_layer][M_perceptron_x][M_input_demension_x];
 //vector< vector< vector< vector< vector<float> > > > > weight;
-float bais[M_layer][M_perceptron_x][M_perceptron_y];
+float bais[M_layer][M_perceptron_x];
 //vector< vector< vector<float> > > bais;
-float input[M_input_n][M_input_demension_x][M_input_demension_y];
+float input[M_input_n][M_input_demension_x];
 //vector< vector<float> > input;
 float label[M_input_n];
-float output[M_layer][M_perceptron_x][M_perceptron_y];
+float output[M_input_n][M_layer][M_perceptron_x];
 
 
 float sigmoid(double x){
@@ -35,8 +36,9 @@ float deff_sigmoid(float input){
 float cost_function(void){
 	float cost = 0;
 	for(int i= 0; i < input_n; i++){
-		cost += (1/2)*(output[i]-label[i])*(output[i]-label[i]);
+		cost += (1/2)*(output[i][layer][1]-label[i])*(output[i][layer][1]-label[i]);
 	}
+	return cost;
 }
 
 void back_propagation(int epoch){
@@ -47,19 +49,17 @@ void back_propagation(int epoch){
 }
 
 int main(){
-	int layer, perceptron_x, perceptron_y, input_demension_x, input_demension_y, input_n;
 	printf("layer 갯수를 입력해주세요.\n");
 	scanf("%d", &layer);
-	printf("layer의 x축 크기를 입력해주세요.\n");
-	scanf("%d", &perceptron_x);
-	printf("layer의 y축 크기를 입력해주세요.\n");
-	scanf("%d", &perceptron_y);
+	for(int i = 1; i <= layer; i++){
+		printf("%d번째 layer의 x축 크기를 입력해주세요.\n", i);
+		scanf("%d", &perceptron_x);
+	}
 	printf("input의 x축 크기를 입력해주세요.\n");
 	scanf("%d", &input_demension_x);
-	printf("input의 y축 크기를 입력해주세요.\n");
-	scanf("%d", &input_demension_y);
 	printf("input데이터의 갯수를 입력해주세요.\n");
 	scanf("%d", &input_n);
+	/*
 	for(int i = 1; i <= layer; i++){
 		for(int j = 1; j <= input_demension_x; j++){
 			for(int k = 1; k <= input_demension_y; k++){
@@ -79,16 +79,24 @@ int main(){
 			printf("\n");
 		}
 	}
+	*/
+	for(int i = 1; i <= layer; i++){
+		for(int j = 1; j <= input_demension_x; j++){
+			for(int k = 1; k <= perceptron_x; k++){
+				weight[i][j][k] = 1.0;
+			}
+		}
+		printf("배열을 초기화 중입니다...\n");
+	}
 	printf("input 데이터를 입력해주세요\n");
 	for(int k = 1; k <= input_n; k++){
 		for(int i = 1; i <= input_demension_x; i++){
-			for(int j = 1; j <= input_demension_y; j++){
-				scanf("%d", &input[k][input_demension_x][input_demension_y]);
-			}
+			scanf("%d", &input[k][input_demension_x]);
 		}
 	}
 	printf("label을 입력해주세요.\n");
 	for(int k = 1; k <= input_n; k++){
 		scanf("%d", &input[k]);
 	}
+	
 }
