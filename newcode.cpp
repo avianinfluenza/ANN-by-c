bb7b3a03fb3fd34631d0bb5a074a0d2fbe21382f
avianinfluenza <64621106+avianinfluenza@users.noreply.github.com>
2021-04-01 11:17:05 +0900

    Update newcode.cpp

--- newcode.cpp
+++ newcode.cpp
@@ -110,7 +110,7 @@ int main(){
     pFile = fopen("D:/iris(150).csv", "r" );
     if( pFile != NULL )
     {   
-    printf("yes");
+    printf("yes\n");
         while( !feof( pFile ) ){
             fgets( str_tmp, 1024, pFile );          
             printf( "%s", str_tmp );
@@ -118,7 +118,8 @@ int main(){
 			char *ptr = strtok(str_tmp, ",");      // " " 공백 문자를 기준으로 문자열을 자름, 포인터 반환
 			while (ptr != NULL)          // 자른 문자열이 나오지 않을 때까지 반복
 			{
-			    printf("%s\n", ptr);         // 자른 문자열 출력
+			    double num = atof(ptr);
+			    printf("%lf\n", num);
 			    ptr = strtok(NULL, ",");      // 다음 문자열을 잘라서 포인터를 반환
 			} 
         }       
